4fd8eb862b9571eb2bacc4cc1e4aac2228bc51c0
Luke Schleder <luke.schleder@gmail.com>
2020-03-06 15:31:07 -0600

    add listener to grab id of item selected and create functions

--- src/app/services/coniferous.service.ts
+++ src/app/services/coniferous.service.ts
@@ -1,9 +1,57 @@
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Tree {
+  name: string;
+  description: string;
+  imageUrl: string;
+
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConiferousService {
 
-  constructor() { }
+  private treesCollection: AngularFirestoreCollection<Tree>;
+
+  private trees: Observable<Tree[]>;
+
+  constructor( db: AngularFirestore) {
+    this.treesCollection = db.collection<Tree>('trees');
+
+    // listens for actions and grabs the id of that object
+    this.trees = this.treesCollection.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      })
+    );
+  }
+  // will get all trees from db
+  getTrees() {
+    return this.trees;
+  }
+  // will get tree of specific id
+  getTree(id) {
+    return this.treesCollection.doc<Tree>(id).valueChanges();
+  }
+  // will update tree name description or imageUrl identified by id
+  updateTree(tree: Tree, id: string) {
+    return this.treesCollection.doc(id).update(tree);
+  }
+  // will add new tree to tree list
+  addTree(tree: Tree) {
+    return this.treesCollection.add(tree);
+  }
+  // will delete tree from tree list
+  removeTree(id) {
+    return this.treesCollection.doc(id).delete();
+  }
+
 }
