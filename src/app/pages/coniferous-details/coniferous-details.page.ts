ea36223b3d3dc17c6457c03ad0bfa06f31f44ebf
Luke Schleder <luke.schleder@gmail.com>
2020-03-07 11:48:41 -0600

    add skeleton and button to home and start details.ts page

--- src/app/pages/coniferous-details/coniferous-details.page.ts
+++ src/app/pages/coniferous-details/coniferous-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Tree, ConiferousService } from 'src/app/services/coniferous.service';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-coniferous-details',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConiferousDetailsPage implements OnInit {
 
-  constructor() { }
+  tree: Tree = {
+    name: 'White Pine',
+    description: 'Monarch of the North',
+    imageUrl: ''
+  }
+
+  treeId = null;
+
+
+  constructor(private coniferousService: ConiferousService, private route: ActivatedRoute, private loadingController: LoadingController) { }
 
   ngOnInit() {
+    this.treeId = this.route.snapshot.params['id'];
+    if (this.treeId) {
+      this.loadTree();
+    }
+  }
+
+  async loadTree() {
+    const loading = await this.loadingController.create({
+      message: 'Loading Trees..'
+    });
+    await loading.present();
+
+    this.coniferousService.getTree(this.treeId).subscribe(res => {
+      loading.dismiss();
+      this.tree = res;
+    });
+  }
+
+  saveTree() {
+
   }
 
 }
