5d1d5b67e9d0274f2d0ec9fabdede85f0bf0ca75
Luke Schleder <luke.schleder@gmail.com>
2020-03-15 17:17:01 -0500

    update home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Tree, ConiferousService } from '../services/coniferous.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,7 @@ export class HomePage implements OnInit {
   // creates array to hold information from db
   trees: Tree[];
 
-  constructor(private coniferousService: ConiferousService) { }
+  constructor(public navCtrl: NavController, private coniferousService: ConiferousService, public fAuth: AngularFireAuth) { }
   // calls getTrees function from coniferousService to populate home page
   ngOnInit() {
     this.coniferousService.getTrees().subscribe(res => {
@@ -20,6 +22,11 @@ export class HomePage implements OnInit {
   // calls removeTree function from coniferousService on home page
   remove(item) {
     this.coniferousService.removeTree(item.id);
+  }
+
+  logout() {
+    this.fAuth.auth.signOut();
+    this.navCtrl.navigateForward('login');
   }
 
 }
